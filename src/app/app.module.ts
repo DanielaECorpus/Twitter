1323bb9be444e8966d4a832d018b2ab8e874dd6a
Oliver Aguilar <oliveraguilar@Olivers-MacBook-Pro.local>
2018-06-29 16:33:18 -0500

    Componente Perfil, final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 //importamos FormsModule para que funcionara el ngModule
 import {FormsModule} from '@angular/forms';
-
 import { AppComponent } from './app.component';
-
 import { PerfilComponent } from './perfil/perfil.component';
-
 import { DashboardLoginPComponent } from './dashboard-login-p/dashboard-login-p.component';
 import { DashboardRegistroComponent } from './dashboard-registro/dashboard-registro.component';
 import { UsuarioComponent } from './usuario/usuario.component';
-
 import { Routes,RouterModule} from '@angular/router';
 
 const appRoutes:Routes=[
   {path:'',component:DashboardLoginPComponent},
   {path:'dashboard-login-p',component:DashboardLoginPComponent},
   {path:'dashboard-registro',component:DashboardRegistroComponent},
-  {path:'usuario',component:UsuarioComponent}
+  {path:'usuario',component:UsuarioComponent},
+  {path:'perfil',component:PerfilComponent}
+
 ]
 
 @NgModule({
