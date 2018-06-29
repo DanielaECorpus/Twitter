759aec012bdbf84d4a1d9f2d0e76c373f6ed7959
Daniela Emiliano <danielaemilianocorpus@gmail.com>
2018-06-29 10:24:21 -0500

    se modifico el package.json, app.module y se agragan 2 componentes para Login y registro. Se incluye Bootstrap y Fontawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardLoginPComponent } from './dashboard-login-p/dashboard-login-p.component';
+import { DashboardRegistroComponent } from './dashboard-registro/dashboard-registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardLoginPComponent,
+    DashboardRegistroComponent
   ],
   imports: [
     BrowserModule
